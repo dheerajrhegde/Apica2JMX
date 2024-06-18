8ddf4e9c50212e5c2634d705e5a19ca36b931aa3
dheeraj hegde <dheeraj.r.hegde@gmail.com>
2024-06-17 21:44:23 -0400

    Updated messages

--- streamlit_app.py
+++ streamlit_app.py
@@ -162,6 +162,5 @@ with col1:
     messages = [HumanMessage(content=apica)]
 
 with col2:
-    st.session_state.abot.graph.bac
-    response = st.session_state.abot.graph.ainvoke({"messages": messages}, st.session_state.thread)
+    response = st.session_state.abot.graph.batch({"messages": messages}, st.session_state.thread)
     st.text_area("JMX XML", key="jmx", value=response['messages'][-1].content, height=600)
